6f3cd77c7faeffa18e78a1bd14bdf41a4c276407
Baet <toktonbekovbaet@gmail.com>
2022-04-12 02:04:50 +0600

    set up main screens

---
+++ app/src/main/java/kg/notesapp/notes/screens/Add.kt
@@ -0,0 +1,9 @@
+package kg.notesapp.notes.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Add(navController: NavHostController) {
+
+}
